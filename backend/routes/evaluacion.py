6d9327a95c070b6711570cb14c5bc1c62ba6b73b
UzielCortez <uziel.cortez@anahuac.mx>
2025-11-20 12:50:32 -0600

    evaluación medio
    Evaluación html ya medio funciona pero el back no

--- backend/routes/evaluacion.py
+++ backend/routes/evaluacion.py
@@ -1,7 +1,56 @@
 from flask import Blueprint, render_template
+from dp import get_connection
 
 evaluacion_bp = Blueprint("evaluacion", __name__)
 
 @evaluacion_bp.route("/evaluacion")
 def evaluacion():
-    return render_template("evaluacion.html")
+
+    # ================== EVALUACION ==================
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM EVALUACION;")
+    evaluaciones = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+
+    # ================== EMPLEADO - EVALUACION ==================
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM `EMPLEADO-EVALUACION`;")
+    empleado_evaluaciones = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+
+    # ================== CAPACITACION ==================
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM CAPACITACION;")
+    capacitaciones = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+
+    # ================== EMPLEADO - CAPACITACION ==================
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM `EMPLEADO-CAPACITACION`;")
+    empleado_capacitaciones = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+
+    # ================== PLAN CARRERA ==================
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM PLAN_CARRERA;")
+    planes_carrera = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+
+    return render_template(
+        "evaluacion.html",
+        evaluaciones=evaluaciones,
+        empleado_evaluaciones=empleado_evaluaciones,
+        capacitaciones=capacitaciones,
+        empleado_capacitaciones=empleado_capacitaciones,
+        planes_carrera=planes_carrera
+    )
